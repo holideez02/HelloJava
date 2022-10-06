914b923f93f0d10528a0e3c0e65fd20c25bd11cb
so02 <holideez02@gmail.com>
2022-10-07 08:52:40 +0900

    1007아침

--- P20221005/src/co/edu/api/StringExe2.java
+++ P20221005/src/co/edu/api/StringExe2.java
@@ -20,10 +20,12 @@ public class StringExe2 {
 	}
 
 
-public static void changeCapital(String msg) {
+public static void changeCapital(String msg) throws IOException {
 	//대문자 -> 소문자, 소문자-> 대문자로 바꾸는 기능 32씩 차이남.
 	// aBcD => AbCd  a97:A65 (-32), b98:B66(-32)
 	//소문자: 97~122, 대문자: 65~90
+	byte[] bytes = new byte[100];
+	
 	
 	
 	}
