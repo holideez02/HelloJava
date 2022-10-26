13ea90864f10416c30147b22748334a7614d33ed
so02 <holideez02@gmail.com>
2022-10-26 11:38:50 +0900

    1026

--- H20221025/src/main/java/co/edu/common/FrontController.java
+++ H20221025/src/main/java/co/edu/common/FrontController.java
@@ -1,5 +1,53 @@
 package co.edu.common;
 
-public class FrontController {
+import java.io.IOException;
+import java.util.HashMap;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.edu.control.MainControl;
+
+public class FrontController extends HttpServlet{
+
+	HashMap<String, Control> controlList;
+	String charset;
+	
+//	//서블릿이 최초로 한번 호출되면 실행되는 init()
+//	@Override
+//	public void init() throws ServletException {
+//		ServletContext sc = this.getServletContext(); //이 메소드로 servletContext를 가지고 옴
+//		sc.getInitParameter("charset"); //이 값으로 셋팅되어져 있는 키 값(UTF-8) 읽어옴.
+//	}
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		charset = config.getInitParameter("charset"); //위와 같다. 둘 중 아무거나 사용.
+		controlList = new HashMap<String, Control>();
+		
+		controlList.put("/main.do", new MainControl());
+		
+	}
+	
+	//서블릿이 호출될 때 마다 실행되는 service()
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String uri = req.getRequestURI(); //uri값을 읽어와서 .. http://localhost:8081/H20221025/main.do 요청정보에서 uri값읽어옴.
+		String context = req.getContextPath(); //H20221025만.
+		String path = uri.substring(context.length()); //main.do만.
+		
+		System.out.println(path);
+		System.out.println(uri);
+		Control subControl = controlList.get(path);
+		subControl.exec(req, resp); //main.do를 호출해서 맵핑되어진 control을 실행.
+	}
+	
+	
+	
+	
 }
