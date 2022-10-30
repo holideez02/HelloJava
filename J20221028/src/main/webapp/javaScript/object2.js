77a39219f843dc597b7184ca18e9805639074341
so02 <holideez02@gmail.com>
2022-10-30 21:21:08 +0900

    1030

--- J20221028/src/main/webapp/javaScript/object2.js
+++ J20221028/src/main/webapp/javaScript/object2.js
@@ -16,58 +16,64 @@ document.addEventListener('DOMContentLoaded', function() {//이벤트이름, 이
 {"id":9,"first_name":"Lester","last_name":"Calverley","email":"lcalverley8@ihg.com","gender":"Male","salary":3977},
 {"id":10,"first_name":"Norris","last_name":"Rey","email":"nrey9@reddit.com","gender":"Male","salary":5130}]`;
 
-// json 타입 => javaScript에서 쓰는 object 타입으로 변환
-let result = JSON.parse(data); //data를 js의 object로 변환.
-console.log(result);
+	// json 타입 => javaScript에서 쓰는 object 타입으로 변환
+	let result = JSON.parse(data); //data를 js의 object로 변환.
+	console.log(result);
 
-// for(String str : personList) 처럼 썼던거를 이제 밑에처럼 써주는것.. 갯수만큼 반복
-// <ul><li>obj</li></ul>
-// <div id="show"></div>
+	// for(String str : personList) 처럼 썼던거를 이제 밑에처럼 써주는것.. 갯수만큼 반복
+	// <ul><li>obj</li></ul>
+	// <div id="show"></div>
 
-const fields = ['id', 'first_name', 'email', 'salary']; //항목을 정해주고
-let tableTag = document.createElement('table');
+	const fields = ['id', 'first_name', 'email', 'salary']; //항목을 정해주고
+	let tableTag = document.createElement('table');
 
-for (row of result){
-	tableTag.appendChild(makeTr(row));
-}
 
-document.getElementById('show').appendChild(tableTag);
-
-for (row of result){ //새로만든..
-	trTag.appendChild(makeTd(row));
-}
-
-document.getElementById('show').appendChild(trTag);
-
-function makeTr(obj){
-	let trTag = document.createElement('tr');
-	return trTag;
-}
-
-function makeTd(obj){
-	let tdTag = document.createElement('td');
-	let str = '';
-	for (field of fields){
-		str += `${field}: ${obj[field]} `;
+	for (row of result) {
+		tableTag.appendChild(makeTr(row));
 	}
-	let txt = document.createTextNode(str);
-	return tdTag;
-	
-}
 
+	document.getElementById('show').appendChild(tableTag);
 
-function makeList(obj){
-	let liTag = document.createElement('li');
-	let str = '';
-	for (field of fields){ //배열에 선언되어져있는 갯수만큼 받아오기
-		str += `${field}: ${obj[field]} `; //id: id속성을 읽어옵니다, first_name: firstname을 읽어옵니다
+	function makeTr(obj) {
+		let trTag = document.createElement('tr');
+		let tdTag = document.createElement('td');
+		let tdTag2 = document.createElement('td');
+		let tdTag3 = document.createElement('td');
+		let tdTag4 = document.createElement('td');
+		let str = '';
+		for (let i = 0; i < fields.length; i++) {
+			str = `${obj.id}`;
+			str2 = `${obj.first_name}`;
+			str3 = `${obj.email}`;
+			str4 = `${obj.salary}`;
+		}
+		let txt = document.createTextNode(str);
+		let txt2 = document.createTextNode(str2);
+		let txt3 = document.createTextNode(str3);
+		let txt4 = document.createTextNode(str4);
+
+		tdTag.appendChild(txt);
+		tdTag2.appendChild(txt2);
+		tdTag3.appendChild(txt3);
+		tdTag4.appendChild(txt4);
+		
+		trTag.appendChild(tdTag);
+		trTag.appendChild(tdTag2);
+		trTag.appendChild(tdTag3);
+		trTag.appendChild(tdTag4);
+		return trTag;
 	}
-	let txt = document.createTextNode(str);
-	liTag.appendChild(txt);
-	
-	return liTag;
-}
 
+	function makeList(obj) {
+		let liTag = document.createElement('li');
+		let str = '';
+		for (field of fields) { //배열에 선언되어져있는 갯수만큼 받아오기
+			str += `${field}: ${obj[field]} `; //id: id속성을 읽어옵니다, first_name: firstname을 읽어옵니다
+		}
+		let txt = document.createTextNode(str);
+		liTag.appendChild(txt);
 
+		return liTag;
+	}
 
 }); 
