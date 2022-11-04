eae97d2ba0b02a5ea3c8158a9c39992bfbeb283f
so02 <holideez02@gmail.com>
2022-11-04 13:53:50 +0900

    1104 시험

---
+++ JavaScriptTest/src/main/webapp/bookList.js
@@ -0,0 +1,174 @@
+//bookList.js
+
+document.addEventListener('DOMContentLoaded', domLoadedFunc);
+
+function domLoadedFunc() {
+
+    fetch('./BookListServlet')
+        .then(result => result.json())
+        .then(showList)
+        .catch(error => console.error(error));
+
+    //head
+    function showList(result) {
+        //타이틀 생성
+        makeHead();
+        //리스트 출력
+        makeList(result);
+    };
+
+    function makeHead() { //타이틀 만들기
+
+        let thead = document.getElementById('title');
+        let tr = document.createElement('tr');
+        let thTag = document.createElement('th');
+        let checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        thTag.append(checkbox);
+        tr.appendChild(thTag);
+
+        checkbox.addEventListener('change', function () {
+            document.querySelectorAll('#list input[type="checkbox"]').forEach(function (check) {
+                check.checked = checkbox.checked;
+            });
+        });
+        let titles = ['도서코드', '도서명', '저자', '출판사', '가격', '삭제'];
+        for (let title of titles) {
+            let thTag = document.createElement('th');
+            let idTxt = document.createTextNode(title);
+            thTag.appendChild(idTxt);
+            tr.appendChild(thTag);
+        }
+        tr.appendChild(thTag);
+        thead.appendChild(tr);
+
+    }
+
+    function makeList(ary = []) { //목록 출력
+
+        let tbody = document.getElementById('list');
+        let fields = ['bookCode', 'bookName', 'author', 'press', 'price'];
+        for (let obj of ary) {
+            let tr = makeTr(obj);
+            tbody.appendChild(tr);
+        }
+
+        function makeTr(row = {}) { //tr생성
+            let tr = document.createElement('tr');
+
+            //체크박스 먼저 생성
+            let td = document.createElement('td');
+            td.style.textAlign = 'center';
+            let check = document.createElement('input');
+            check.addEventListener('change', function () {
+                console.log(this.checked);
+            })
+            check.setAttribute('type', 'checkbox');
+            td.append(check);
+            tr.append(td);
+
+            //db의 데이터들 생성
+            for (let field of fields) {
+                let td = document.createElement('td');
+                let txt = document.createTextNode(row[field]);
+                td.append(txt);
+                tr.append(td);
+                tbody.append(tr);
+            }
+            //화면에서 삭제
+            td = document.createElement('td')
+            let btn = document.createElement('button');
+            btn.setAttribute('id', 'delete')
+            // btn.addEventListener('click', function () {
+            //     this.parentElement.parentElement.remove();
+            // });
+            let txt = document.createTextNode('삭제');
+            td.appendChild(btn);
+            btn.appendChild(txt);
+            tr.appendChild(td);
+
+            return tr;
+        }
+        //////////////////////////추가//////////////////////////
+        document.querySelector('button[type=button]').addEventListener('click', addFunc);
+        
+        function addFunc(){ //데이터 추가
+            let bc = document.getElementById('bookCode').value;
+            let bn = document.getElementById('bookName').value;
+            let au = document.getElementById('author').value;
+            let pr = document.getElementById('press').value;
+            let price = document.getElementById('price').value;
+            let check = bc && bn && au && pr && price;
+			if (!check) {
+                alert('필수항목입니다.')
+				return false;
+			}
+
+            let data = 'bookCode=' + bc + '&bookName=' + bn + '&author=' + au + '&press=' + pr + '&price=' + price;
+
+            fetch('./BookAddServlet', {
+                method: 'post',
+                headers: {
+                    'Content-type': 'application/x-www-form-urlencoded'
+                },
+                body: data
+            })
+            .then(result => result.json())
+            .then(addCallback)
+            .catch(error => console.error(error));
+            
+            return false;
+        }
+
+        function addCallback(result) {
+            document.querySelector('#list').append(makeTr(result));
+
+            //화면 입력 초기화
+            document.getElementById('bookCode').value = '';
+            document.getElementById('bookName').value = '';
+            document.getElementById('author').value = '';
+            document.getElementById('press').value = '';
+            document.getElementById('price').value = '';
+        }
+
+        //////////////////////////삭제//////////////////////////
+        //document.querySelector('button[type=button]').addEventListener('click', delFunc);
+        document.querySelector('#delete').addEventListener('click', delFunc);
+
+        function delFunc(){
+            console.log('삭제실행')
+            let bookid = this.parentElement.parentElement.getAttribute('bookCode');
+            let data = bookid;
+            fetch('./BookDelServlet', {
+                method: 'post',
+                headers: {
+                    'Content-type': 'application/x-www-form-urlencoded'
+                },
+                body: data
+            })
+            .then(result => result.json())
+            .then(delCallback)
+            .catch(error => console.error(error));
+
+            function delCallback(result){
+                console.log(result);
+            }
+
+            //this.parentElement.parentElement.remove(); :화면에서 삭제..
+
+        }
+        
+        ////////////////////////선택삭제//////////////////////////////
+        document.getElementById('checkDel').addEventListener('click', deleteCheckedFunc);
+
+        function deleteCheckedFunc(){
+            let trs = document.querySelectorAll('#list>tr');
+            for(let i=0; i<trs.length; i++){
+                if(trs[i].children[0].firstElementChild.checked){
+                    trs[i].remove();
+                }
+            }
+        }
+
+    }
+} //end of domLoadedFunc
